5066537e26ed25d9a459785d4b67504237b41fc9
tosiq ahmed <m.tosiqshafiq@yahoo.com>
2021-04-06 21:46:15 +0500

    work done finaly

---
+++ htmlheperloselytype/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using htmlheperloselytype.Models;
+
+namespace htmlheperloselytype.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        // GET: Home
+        public ActionResult Index()
+        {
+            //var t = new List<Class1>();
+
+            //var user1 = new Class1();
+
+            //user1.name = "Tosiq";
+            //user1.email = "m.tosiqshafiq@yahoo.com";
+            //user1.password = "1234";
+            //user1.Date = " hhhh";
+
+            //var user2 = new Class1();
+
+            //user2.name = "Tosiq";
+            //user2.email = "m.tosiqshafiq@yahoo.com";
+            //user2.password = "1234";
+            //user2.Date = " hhhh";
+
+            //t.Add(user1);
+            //t.Add(user2);
+
+            //ViewBag.a = t;
+
+
+
+            return View();
+        }
+       
+   
+       
+    }
+}
---
+++ htmlheperloselytype/Models/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace htmlheperloselytype.Models
+{
+    public class Class1
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string Date { get; set; }
+        public TimeZone Time { get; set; }
+        public string gender { get; set; }
+
+        public string courses { get; set; }
+
+        public string country { get; set; }
+        public string image { get; set; }
+
+
+
+    }
+}
